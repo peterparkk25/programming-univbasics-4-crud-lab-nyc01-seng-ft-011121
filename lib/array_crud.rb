5dab6bf04546ab49da8cf9d15aedf44a773a71aa
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-29 23:42:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  house_pets = ["cat", "dog", "am", "wow"]
+  house_pets = ["wow", "dog", "am", "learning"]
   house_pets[0]
 end
 
