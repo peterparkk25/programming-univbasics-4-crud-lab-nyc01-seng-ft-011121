532af080bae085114f225f6543bcb4da6c718a96
Peter Park 2.0 <peterparkk25+@gmail.com>
2020-12-29 23:44:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  house_pets = ["wow", "dog", "am", "arrays!"]
+  house_pets[3] = "huh"
 end
